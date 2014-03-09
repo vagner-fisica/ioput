9167c19c046ecbfdc0564d5a1cf8f3472277a40b
Vagner Bessa <vagner.fisica@gmail.com>
2014-03-09 18:50:09 -0300

    1.0/09.03.2014:1850

--- main.cpp
+++ main.cpp
@@ -14,12 +14,33 @@ void print(T data, bool endline = false){
 
 int main(){
 
-	IOput<int> a("TESTING");
-	print("This is my test folder: ");
-	print(a.cwd(),true);
+	IOput<double> a("TESTING");
 
-	a.change_cwd("TESTING02");
-	print("This is my NEW test folder: ");
-	print(a.cwd(),true);
+	double x[5] = {0.1,0.2,0.3,0.4,0.5};
+	double y[5] = {0.01,0.02,0.03,0.04,0.05};
+	double z = 0.1;
+	string fname("file");
+	a.format_out_data("scientific", 7);
+	a.save_x(z,z,fname);
+
+//	double x[5] = {0.1,0.2,0.3,0.4,0.5};
+//	string fname("file");	
+//	a.save_x(x,5,fname,"\t",-1,-1,false,false);
+	
+//	double x = 1.0e-3;
+//	string fname("file");
+//	a.format_out_data("scientific", 7);
+//	a.save_x(x,fname,1,1,true,true);
+//	a.format_out_data("fixed", 7);
+//	a.save_x(x,fname+"2",1,4,true,true);
+
+//	print("This is my test folder: ");
+//	print(a.cwd(),true);
+
+//	a.change_cwd("TESTING02");
+//	print("This is my NEW test folder: ");
+//	print(a.cwd(),true);
+	
+//	print(a.lzeros(4,1),true);
 	
 }
