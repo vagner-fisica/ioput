7475881315ef67a2833d6cc72ecc4c315ad2a7a1
Vagner Bessa <vagner.fisica@gmail.com>
2014-03-10 14:50:55 -0300

    1.0/10.03.2014:1450

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 template <class T> 
-void print(T data, bool endline = false){
+void print(T data, bool endline = true){
 	if (endline){
 		cout << data << endl;
 	}
@@ -16,12 +16,18 @@ int main(){
 
 	IOput<double> a("TESTING");
 
-	double x[5] = {0.1,0.2,0.3,0.4,0.5};
-	double y[5] = {0.01,0.02,0.03,0.04,0.05};
-	double z = 0.1;
-	string fname("file");
-	a.format_out_data("scientific", 7);
-	a.save_x(z,z,fname);
+	double x = 0.03;
+	a.save_x(x,"test",-1,-1,false,true);
+
+
+//	double x[5] = {0.1,0.2,0.3,0.4,0.5};
+//	double y[5] = {0.01,0.02,0.03,0.04,0.05};
+//	double z[5] = {1.0,2.0,3.0,4.0,5.0};
+//	string fname("filexy");
+//	a.save_xy(x,y,5,fname);
+//	fname = "filexyz";
+//	a.format_out_data("fixed",3);
+//	a.save_xyz(x,y,z,5,fname);
 
 //	double x[5] = {0.1,0.2,0.3,0.4,0.5};
 //	string fname("file");	
@@ -38,9 +44,9 @@ int main(){
 //	print(a.cwd(),true);
 
 //	a.change_cwd("TESTING02");
-//	print("This is my NEW test folder: ");
-//	print(a.cwd(),true);
+//	print("This is my NEW test folder: ",false);
+//	print(a.cwd());
 	
-//	print(a.lzeros(4,1),true);
+//	print(a.lzeros(4,1));
 	
 }
