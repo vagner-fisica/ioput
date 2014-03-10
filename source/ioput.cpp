758f2f8e1193dfef47afe056df33001cdecdc4ad
Vagner Bessa <vagner.fisica@gmail.com>
2014-03-10 14:50:28 -0300

    1.0/10.03.2014:1450

--- source/ioput.cpp
+++ source/ioput.cpp
@@ -12,9 +12,7 @@ IOput<T>::IOput(const char* dir_name){
 //	Default file extension
 	ext = ".dat";
 	cwd_ = new string(dir_name);
-	string_s aux;
-	aux << "mkdir -p " << *cwd_ << "/";
-	system(aux.str().c_str());
+	mkdir(*cwd_);
 }
 template <class T>
 IOput<T>::IOput(string dir_name){
@@ -33,11 +31,11 @@ string IOput<T>::cwd(){
 template <class T>
 string IOput<T>::full_cwd(){
 	system("echo $PWD > .temp_full_cwd_path");
-	ifstream in(".temp_full_cwd_path");
+	inf.open(".temp_full_cwd_path");
 	string aux;
-	getline(in,aux);
+	getline(inf,aux);
 	aux += string("/") + *cwd_;
-	in.close();
+	inf.close();
 	system("rm .temp_full_cwd_path");
 	return aux;
 }
@@ -49,14 +47,7 @@ void IOput<T>::change_cwd(string new_dir_name){
 	*cwd_ = new_dir_name;
 	mkdir(*cwd_);
 }
-//Creates a new folder at the root
-//directory
-template <class T>
-void IOput<T>::mkdir(string s){
-	string_s aux;
-	aux << "mkdir -p " + s + "/";
-	system(aux.str().c_str());	
-}
+
 //Store a simple data
 template <class T>
 void IOput<T>::save_x(T x,
@@ -65,38 +56,12 @@ void IOput<T>::save_x(T x,
 					  int nzeros,
 					  bool append,
 					  bool new_sub_folder){
-//	If data is to be store in a 
-//	new folder at the root directory				  
-	if(new_sub_folder){
-		string new_folder = *cwd_ + "/" + fname + "/";
-		mkdir(new_folder);
-		fname = new_folder + fname;
-	}
-//	If file has to de indexed with a 
-//	ending integer 'idx'
-	if(idx >= 0){
-//		If the ending integer should be
-//		followed by leading zeros		
-		if(nzeros >= 0){
-			fname += "_" + lzeros(nzeros,idx);
-		}
-		else{
-			fname += "_" + idx;
-		}
-	}
-	fname += ext;
-//	If new data should be appended to the file	
-	if(append){
-		outf.open(fname.c_str(), std::ios::out | std::ios::app);
-	}
-	else{
-		outf.open(fname.c_str(), std::ios::out);
-	}
-	if(outf.good()){
-//		Data has '\n' at the end
+
+	def_par_handler(fname,idx,nzeros,append,new_sub_folder);
+	
+	if (outf.good()) {
 		outf << x << std::endl;
-	}
-	else{
+	} else {
 		std::cout << "ERROR: could not write into file "\
 				  << fname << "." << std::endl;
 	}
@@ -114,35 +79,18 @@ void IOput<T>::save_x(T *x,
 					  bool append,
 					  bool new_sub_folder){
 					  
-	if(new_sub_folder){
-		string new_folder = *cwd_ + "/" + fname + "/";
-		mkdir(new_folder);
-		fname = new_folder + fname;
-	}
-	if(idx >= 0){
-		if(nzeros >= 0){
-			fname += "_" + lzeros(nzeros,idx);
-		}
-		else{
-			fname += "_" + idx;
-		}
-	}
-	fname += ext;
-	if(append){
-		outf.open(fname.c_str(), std::ios::out | std::ios::app);
-	}
-	else{
-		outf.open(fname.c_str(), std::ios::out);
-	}
-	if(outf.good()){
+	def_par_handler(fname,idx,nzeros,append,new_sub_folder);
+	
+	if (outf.good()) {
 		for (unsigned int i = 1; i <= xrange; i++){
-			if(i < xrange)outf << *x << delimiter;
-			else outf << *x;
+			if (i < xrange) {
+				outf << *x << delimiter;
+			} else {
+				outf << *x;
+			}
 			x++;
 		}
-		
-	}
-	else{
+	} else {
 		std::cout << "ERROR: could not write into file "\
 				  << fname << "." << std::endl;
 	}
@@ -158,40 +106,14 @@ void IOput<T>::save_xy(T x, T y,
 					  int nzeros,
 					  bool append,
 					  bool new_sub_folder){
-//	If data is to be store in a 
-//	new folder at the root directory				  
-	if(new_sub_folder){
-		string new_folder = *cwd_ + "/" + fname + "/";
-		mkdir(new_folder);
-		fname = new_folder + fname;
-	}
-//	If file has to de indexed with a 
-//	ending integer 'idx'
-	if(idx >= 0){
-//		If the ending integer should be
-//		followed by leading zeros		
-		if(nzeros >= 0){
-			fname += "_" + lzeros(nzeros,idx);
-		}
-		else{
-			fname += "_" + idx;
-		}
-	}
-	fname += ext;
-//	If new data should be appended to the file	
-	if(append){
-		outf.open(fname.c_str(), std::ios::out | std::ios::app);
-	}
-	else{
-		outf.open(fname.c_str(), std::ios::out);
-	}
-	if(outf.good()){
-//		Data has '\n' at the end
+
+	def_par_handler(fname,idx,nzeros,append,new_sub_folder);
+
+	if (outf.good()) {
 		outf << x << delimiter << y << std::endl;
-	}
-	else{
+	} else {
 		std::cout << "ERROR: could not write into file "\
-				  << fname << "." << std::endl;
+		 		  << fname << "." << std::endl;
 	}
 	outf.close();
 }
@@ -208,35 +130,66 @@ void IOput<T>::save_xy(T *x, T *y,
 					  bool append,
 					  bool new_sub_folder){
 					  
-	if(new_sub_folder){
-		string new_folder = *cwd_ + "/" + fname + "/";
-		mkdir(new_folder);
-		fname = new_folder + fname;
-	}
-	if(idx >= 0){
-		if(nzeros >= 0){
-			fname += "_" + lzeros(nzeros,idx);
-		}
-		else{
-			fname += "_" + idx;
-		}
-	}
-	fname += ext;
-	if(append){
-		outf.open(fname.c_str(), std::ios::out | std::ios::app);
-	}
-	else{
-		outf.open(fname.c_str(), std::ios::out);
-	}
-	if(outf.good()){
+	def_par_handler(fname,idx,nzeros,append,new_sub_folder);
+
+	if (outf.good()) {
 		for (unsigned int i = 1; i <= xrange; i++){
 			outf << *x << delimiter << *y << std::endl;
 			x++;
 			y++;
 		}
 		
+	} else {
+		std::cout << "ERROR: could not write into file "\
+				  << fname << "." << std::endl;
 	}
-	else{
+	outf.close();
+}
+
+//Store a simple data of the type (x,y,z)
+template <class T>
+void IOput<T>::save_xyz(T x, T y, T z,
+					  string fname,
+					  string delimiter,
+					  int idx ,
+					  int nzeros,
+					  bool append,
+					  bool new_sub_folder){
+
+	def_par_handler(fname,idx,nzeros,append,new_sub_folder);
+
+	if (outf.good()) {
+		outf << x << delimiter << y << delimiter << z << std::endl;
+	} else {
+		std::cout << "ERROR: could not write into file "\
+				  << fname << "." << std::endl;
+	}
+	outf.close();
+}
+//Store two simple arrays of data of the
+//type (x,y,z). Both arrays has to have the 
+//same number of elements ('xrange')
+template <class T>
+void IOput<T>::save_xyz(T *x, T *y, T *z,
+					  int xrange,
+					  string fname,
+					  string delimiter,
+					  int idx,
+					  int nzeros,
+					  bool append,
+					  bool new_sub_folder){
+					  
+	def_par_handler(fname,idx,nzeros,append,new_sub_folder);
+	
+	if (outf.good()) {
+		for (unsigned int i = 1; i <= xrange; i++){
+			outf << *x << delimiter << *y << delimiter << *z << std::endl;
+			x++;
+			y++;
+			z++;
+		}
+		
+	} else {
 		std::cout << "ERROR: could not write into file "\
 				  << fname << "." << std::endl;
 	}
@@ -245,15 +198,19 @@ void IOput<T>::save_xy(T *x, T *y,
 
 template <class T>
 void IOput<T>::format_out_data(string floatfield, int precision = 0){
-	if(precision > 0){
+	if (precision > 0) {
 		outf.precision(precision);
 	}
-	if(!floatfield.compare("scientific")){
+	
+	if (!floatfield.compare("scientific")) {
 		outf << std::scientific;
-	}
-	if(!floatfield.compare("fixed")){
+	} else if (!floatfield.compare("fixed")) {
 		outf << std::fixed;
+	} else {
+		std::cout << "ERROR: Unkown format type. Use either \'scientific\'\
+					  or \'fixed\'. using \'default\' instead." << std::endl;
 	}
+	
 }
 
 template <class T>
@@ -264,6 +221,58 @@ string IOput<T>::lzeros(int nzeros, int n){
 	aux >> str;
 	return str; 
 }
+
+//Creates a new folder at the root
+//directory
+template <class T>
+void IOput<T>::mkdir(string s){
+	string_s aux;
+	aux << "mkdir -p " + s + "/";
+	system(aux.str().c_str());	
+}
+
+//Creates a new sub-folder in the
+//cwd directory
+template <class T>
+void IOput<T>::mk_subdir(string &fname){
+	string new_folder = *cwd_ + "/" + fname + "/";
+	mkdir(new_folder);
+	fname = new_folder + fname;
+}
+
+//Handles default parameters of
+//save_ functions.
+template <class T>
+void IOput<T>::def_par_handler(string &fname,
+							   int idx,
+							   int nzeros,
+							   bool append,
+							   bool new_sub_folder){
+//	If data is to be store in a 
+//	new folder at the root directory				  
+	if (new_sub_folder) {
+		mk_subdir(fname);
+	}
+//	If file has to de indexed with a 
+//	ending integer 'idx'
+	if (idx >= 0) {
+//		If the ending integer should be
+//		followed by leading zeros		
+		if (nzeros >= 0) {
+			fname += "_" + lzeros(nzeros,idx);
+		} else {
+			fname += "_" + idx;
+		}
+	}
+	fname += ext;
+//	If new data should be appended to the file	
+	if (append) {
+		outf.open(fname.c_str(), std::ios::app);
+	} else {
+		outf.open(fname.c_str());
+	}
+}
+
 template class IOput<int>;
 template class IOput<float>;
 template class IOput<double>;
